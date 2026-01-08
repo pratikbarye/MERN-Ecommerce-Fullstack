fe3fe4c6f3174a2a8d63e8cda2fefa114d285471
pratikbarye <129965133+pratikbarye@users.noreply.github.com>
2026-01-09 03:06:18 +0530

    Change default PORT from 8080 to 5000

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const cloudinary = require('cloudinary');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
-const PORT = process.env.PORT || 8080; // 🚨 REQUIRED FOR RAILWAY
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
