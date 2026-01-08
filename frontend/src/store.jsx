936eff940b02ea05f621888bd8ed8be82a4af27d
pratikbarye <129965133+pratikbarye@users.noreply.github.com>
2026-01-09 04:11:55 +0530

    Fix Redux store configuration for Vite build

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,59 +1,19 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
+import { configureStore } from "@reduxjs/toolkit";
+
+// Import your reducers
+import authReducer from "./reducers/authReducer";
+import productReducer from "./reducers/productReducer";
+import cartReducer from "./reducers/cartReducer";
+import orderReducer from "./reducers/orderReducer";
 
 const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== 'production',
-});
-
-export default store;
-
-import { addNewProductReducer, allProductReviewReducer, deleteProductReviewReducer, deleteUpdateProductReducer, newReviewReducer, productReducer, singleProductReducer } from './reducers/productReducer';
-import { allUserReducer, profileReducer, singleUserDetailReducer, updateAndDeleteUserReducer, userReducer } from './reducers/userReducer';
-import { cartReducre } from './reducers/cartReducre';
-import { allOrderReducer, myOrderReducer, newOrderReducer, singleOrderDetailsReducer, updateAndDeleteOrderReducer } from './reducers/orderReducer';
-
-const reducer = combineReducers({
+  reducer: {
+    auth: authReducer,
     products: productReducer,
-    singleProduct: singleProductReducer,
-    user: userReducer,
-    profile: profileReducer,
-    cart: cartReducre,
-    newOrder: newOrderReducer,
-    myOrders: myOrderReducer,
-    singleOrderDetails: singleOrderDetailsReducer,
-    newReview: newReviewReducer,
-    newProduct: addNewProductReducer,
-    deleteUpdateProduct: deleteUpdateProductReducer,
-    allOrder: allOrderReducer,
-    updateAndDeleteOrder: updateAndDeleteOrderReducer,
-    allUser: allUserReducer,
-    singleUserDetail: singleUserDetailReducer,
-    updateAndDeleteUser:updateAndDeleteUserReducer,
-    allProductReview:allProductReviewReducer,
-    deleteProductReview:deleteProductReviewReducer,
+    cart: cartReducer,
+    orders: orderReducer,
+  },
+  devTools: import.meta.env.MODE !== "production",
 });
-
-let initialState = {
-    cart: {
-        cartItems: localStorage.getItem("cartItems")
-            ? JSON.parse(localStorage.getItem("cartItems"))
-            : [],
-        shippingInfo: localStorage.getItem("shippingInfo")
-            ? JSON.parse(localStorage.getItem("shippingInfo"))
-            : {},
-    }
-};
-
-const middleware = [thunk];
-
-
-const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-);
 
 export default store;
