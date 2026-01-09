1e0501b07d6f955897364ea0aafd911412c5f726
pratikbarye <129965133+pratikbarye@users.noreply.github.com>
2026-01-09 11:27:57 +0530

    Fix SmallSpinner import path for Vercel build

--- frontend/src/Pages/Payment/Payment.jsx
+++ frontend/src/Pages/Payment/Payment.jsx
@@ -13,7 +13,8 @@ import { useAlert } from 'react-alert'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 import { clearError, createOrder } from '../../actions/orderAction';
-import Small from '../../Components/smallSpiner/SmallSpiner'
+// import Small from '../../Components/smallSpiner/SmallSpiner'
+import SmallSpinner from "../../Components/smallSpinner/SmallSpinner";
 const Payment = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
