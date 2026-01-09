bba21b72aa4d0762c4532d5a6cced8f4f19e9b87
pratikbarye <129965133+pratikbarye@users.noreply.github.com>
2026-01-09 11:45:53 +0530

    Update cartReducer.jsx

--- frontend/src/reducers/cartReducer.jsx
+++ frontend/src/reducers/cartReducer.jsx
@@ -1,6 +1,6 @@
 import { ADD_TO_CART, REMOVE_ITEM_FROM_CART, SAVE_SHIPPING_INFO } from "../constants/cartConstants";
 
-const cartReducre = (state = { cartItems: [], shippingInfo: {} }, action) => {
+const cartReducer = (state = { cartItems: [], shippingInfo: {} }, action) => {
     switch (action.type) {
         case ADD_TO_CART:
             const item = action.payload;
